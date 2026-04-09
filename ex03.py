09e4ef6d2a29633422b05827774fa3a818c02d99
Jaozin <jeipi7007@gmail.com>
2026-04-09 20:20:52 -0300

    Exercicio 3 - Atualização para nao ser possivel dividir por 0

--- ex03.py
+++ ex03.py
@@ -3,7 +3,15 @@ class Calculadora:
         return a+b
     def subtrair(self, a, b):
         return a-b
-
+    def multiplicar(self, a, b):
+        return a*b
+    def dividir(self, a, b):
+        if b == 0 or a == 0:
+            return("Não é possivel dividir por zero!")
+        else:
+            return(a/b)
 calc = Calculadora()
 print(calc.adicionar(10, 6))
-print(calc.subtrair(10, 5))
+print(calc.subtrair(10, 5   ))
+print(calc.multiplicar(10, 5))
+print(calc.dividir(10, 5))
